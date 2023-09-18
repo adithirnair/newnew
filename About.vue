b70449f71ba500f024127114979ee12554d453d2
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-18 23:22:02 +1000

    Update About.vue

--- About.vue
+++ About.vue
@@ -380,6 +380,26 @@ a {
     },
   };
   </script>
+
+
+//REACTIVITY FUNDAMENTALS
+
+<template>
+    <div>
+      <p>Count: {{ count }}</p>
+      <button @click="increment">Increment</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { ref } from 'vue';
+  
+  const count = ref(0);
+  
+  const increment = () => {
+    count.value++;
+  };
+  </script>
   
   
   
