930902a8f17d75205df9e57aacaccdce83b71880
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-18 23:23:16 +1000

    Update About.vue

--- About.vue
+++ About.vue
@@ -441,6 +441,33 @@ a {
   </script>
  
   
+  //watchers 
+
+<template>
+    <div>
+      <input type="text" v-model="textToWatch" />
+    </div>
+  </template>
+  
+  <script>
+  import { ref, watch } from 'vue';
+  
+  export default {
+    setup() {
+      // Data property to watch
+      const textToWatch = ref('');
+  
+      // Watcher
+      watch(textToWatch, (newValue, oldValue) => {
+        console.log(`Text changed from "${oldValue}" to "${newValue}"`);
+      });
+  
+      return {
+        textToWatch,
+      };
+    },
+  };
+  </script>
   
   
   
