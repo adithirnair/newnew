62462a84c8840373e5f41ba8ea465266bab49e90
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-18 23:14:44 +1000

    Update About.vue

--- About.vue
+++ About.vue
@@ -4,3 +4,38 @@
       <router-link to="/">Back to Home</router-link>
     </div>
   </template>
+
+  <template>
+    <div>
+      <h2>{{ title }}</h2>
+      <button @click="emitEvent">Emit Event</button>
+      <slot></slot>
+    </div>
+  </template>
+  //childcomponents
+  <script>
+  import { defineProps, defineEmits } from 'vue';
+  
+  export default {
+    props: {
+      title: String,
+    },
+    setup() {
+      // Define props to access them in the setup function
+      const props = defineProps(['title']);
+  
+      // Emit custom event
+      const emit = defineEmits();
+  
+      const emitEvent = () => {
+        emit('custom-event', 'Data from child component');
+      };
+  
+      return {
+        props, // Make props accessible in the setup function
+        emitEvent,
+      };
+    },
+  };
+  </script>
+  
