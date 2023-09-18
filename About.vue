15b4d44c8e91e88aaf0bb2cdf0f132488721ff68
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-18 23:21:28 +1000

    Update About.vue

--- About.vue
+++ About.vue
@@ -347,6 +347,40 @@ a {
     },
   };
   </script>
+
+
+//parent component
+
+<template>
+    <div>
+      <child-component :title="childTitle" @custom-event="handleChildEvent">
+        <p>Slot content goes here</p>
+      </child-component>
+    </div>
+  </template>
+  
+  <script>
+  import ChildComponent from './ChildComponents.vue';
+  
+  export default {
+    components: {
+      ChildComponent,
+    },
+    setup() {
+      const childTitle = 'Child Component Title';
+  
+      const handleChildEvent = (data) => {
+        console.log(`Received event from child component with data: ${data}`);
+      };
+  
+      return {
+        childTitle,
+        handleChildEvent,
+      };
+    },
+  };
+  </script>
+  
   
   
   
