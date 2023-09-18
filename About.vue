29a8b7303457250c82c80d07da76c0d95e64422b
adithirnair <139519572+adithirnair@users.noreply.github.com>
2023-09-18 23:18:15 +1000

    Update About.vue

--- About.vue
+++ About.vue
@@ -136,6 +136,27 @@
   };
   </script>
 
+//event handling
+
+<template>
+    <button @click="handleClick">Click me</button>
+  </template>
+  
+  <script>
+  export default {
+    setup() {
+      const handleClick = () => {
+        // Handle the click event
+        console.log('Button clicked');
+      };
+  
+      return {
+        handleClick,
+      };
+    },
+  };
+  </script>
+  
   
   
   
